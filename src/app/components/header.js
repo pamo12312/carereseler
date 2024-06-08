4f01b224046802382d03ebf058fa6df3b19458d6
patrik moravec <patri.moraevc@email.cz>
2024-06-08 10:03:42 +0200

    navbar

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+
 import Logo from "@/app/components/logo";
 import Contact from "@/app/components/contact";
 export default function Header() {
@@ -6,6 +6,10 @@ export default function Header() {
         <div className="flex justify-between">
 <Logo></Logo>
             <Contact></Contact>
+
+
+
+
 
 
 
