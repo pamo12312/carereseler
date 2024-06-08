8d76d9359c1052d678da46ddf5e8cc00ace6403d
patrik moravec <patri.moraevc@email.cz>
2024-06-08 10:02:36 +0200

    navbar

---
+++ src/app/components/contact.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Contact() {
+    return (
+        <div>
+
+            <h1 className="text-3xl  font-bold underline">
+              kontaktujte nás
+            </h1>
+        </div>
+    );
+}
---
+++ src/app/components/header.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Logo from "@/app/components/logo";
+import Contact from "@/app/components/contact";
+export default function Header() {
+    return (
+        <div className="flex justify-between">
+<Logo></Logo>
+            <Contact></Contact>
+
+
+        </div>
+    );
+}
